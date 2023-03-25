85e0f43af3d82f4f89db2013e02af88eadcfb6df
admin <litrax@bk.ru>
2023-03-26 00:43:35 +0300

    Небольшие правки

--- bot_v1.py
+++ bot_v1.py
@@ -1,7 +1,8 @@
 import logging
 
+import telegram.ext
 
-from config import bot_token
+from config import bot_token, bot_name
 from telegram import Update
 from telegram.ext import (
     Application,
@@ -13,25 +14,32 @@ from telegram.ext import (
 
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update,
+                context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued"""
     user = update.effective_user
     await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я {123}. Напишите мне что-нибудь, и я пришлю это назад!",
+        f"Привет {user.mention_html()}! \
+Я {bot_name}. Напишите мне что-нибудь, и я пришлю это назад!",
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def help_command(update: Update,
+                       context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued"""
-    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+    await update.message.reply_text(
+        "Я пока не умею помогать... Я только ваше эхо."
+    )
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def echo(update: Update,
+               context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message"""
     await update.message.reply_text(
         update.message.text if update.message.text[0] != '/' else 'неизвестная команда'
@@ -42,6 +50,7 @@ def main() -> None:
     """Start the bot"""
 
     application = Application.builder().token(bot_token).build()
+    # extbot = telegram.ext.ExtBot(token=bot_token)
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 bot_token = '5876207271:AAHFctyta4siexGi8SaMw73gNWqK5l2zZ4s'
+bot_name = 'Whimsy'
