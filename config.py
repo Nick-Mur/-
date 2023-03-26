964b113c365c5f1cf0bfb139d9e3ba34c7b6eaac
admin <litrax@bk.ru>
2023-03-26 22:23:35 +0300

    Автоматизация

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import requests
 bot_token = '5876207271:AAHFctyta4siexGi8SaMw73gNWqK5l2zZ4s'
-bot_name = 'Whimsy'
-chat_id = 5876207271
+MAIN_URL = f'https://api.telegram.org/bot{bot_token}'
+
+r = requests.get(f'{MAIN_URL}/getMe').json()
+# print(r)
+
+bot_name = r['result']['first_name']
+chat_id = r['result']['id']
